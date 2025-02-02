dcb8eb72d90832099c16138a7ca852b60ce512a2
FranklinS734 <martinc3@usf.edu>
2025-02-01 23:28:01 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,13 @@
+// Task 1 - Product Price Management Scenario
+// Declaring an array for "prices" with 5 values which will represent different gym prices
+let prices = [10.99,15.99,21.99,25,30];
+
+// This will show a new price to the array using .push() which allows us to do so. 
+prices.push(26); //added new gym price
+
+// Removing the first price from the array
+prices.shift();
+
+// Log the updated price list
+console.log("Updated Gym Membership Prices:", prices);
+
