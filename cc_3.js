545bb774ff44aa1a7412e6394b1ae5531ca1fd71
FranklinS734 <martinc3@usf.edu>
2025-02-01 23:36:06 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -11,3 +11,17 @@ prices.shift();
 // Log the updated price list
 console.log("Updated Gym Membership Prices:", prices);
 
+// Task 2 - Modifying Customer Orders Scenario
+
+// Declaring an array "orders" representing the quantity of creatine supplement ordered
+let orders = [6, 9, 5, 7, 3];
+
+// Increasing the third order quantity by 5 (customer requested an update to their order)
+orders[2] += 5;
+
+// Calculate the total number of all orders using .reduce() to sum the array values
+let totalOrders = orders.reduce((total, qty) => total + qty, 0);
+
+
+console.log("Updated Creatine Orders:", orders);
+console.log("Total Creatine Ordered:", totalOrders);
